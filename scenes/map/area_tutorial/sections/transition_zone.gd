f6659b73324943836b597c82f446cb4b85ad1824
Christian Saavedra <chrisosami86@gmail.com>
2025-11-03 21:42:04 -0500

    Terminando las sesiones del primer nivel

--- scenes/map/area_tutorial/sections/transition_zone.gd
+++ scenes/map/area_tutorial/sections/transition_zone.gd
@@ -5,5 +5,6 @@ extends Area2D
 
 
 func _on_area_entered(area: Area2D) -> void:
+	print(area.global_position)
 	if area.name == "HurtBox": # o "Player"
 		get_tree().get_first_node_in_group("main").fade_to_black_and_change_scene(next_section_path, player_spawn_position)
